4c2be3cb8f0b1ac2e12de105b8a6a8c8eff98f4f
Megjafari <megberserker@gmail.com>
2026-04-03 23:03:30 +0200

    feat: add Auth0 authentication

---
+++ src/auth/Auth0Provider.tsx
@@ -0,0 +1,25 @@
+import { Auth0Provider } from '@auth0/auth0-react'
+import type { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export default function Auth0ProviderWithNavigate({ children }: Props) {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: audience,
+      }}
+    >
+      {children}
+    </Auth0Provider>
+  )
+}
